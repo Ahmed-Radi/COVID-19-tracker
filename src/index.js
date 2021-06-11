3c1da9cb943dc8391a94018f800f3afbb8604687
Ahmed Radi <ahmedradi743@gmail.com>
2021-06-11 16:55:05 +0200

    upload project files
    Basic file structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById('root'));/**the Second parameter is the element you want to render on the App component -> this ID->' root ' I got from index.html in a public file*/
